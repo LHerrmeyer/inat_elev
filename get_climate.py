13aba51a98e2e055ad11ddea0eeaf9120e9440bd
Logan H <logern5@gmail.com>
2021-03-06 11:35:16 -0600

    Give example in get_climate.py

--- get_climate.py
+++ get_climate.py
@@ -1,22 +1,35 @@
 import noaa_api as na
+import statistics
 
+state = "Minnesota"
+keyword = "minneapolis st"
+dset = "GSOY"
 
-state_id = na.get_state_id("Minnesota")
-q_dict = {"locationid":state_id, "datasetid":"GSOM","enddate":"2001-01-01"}
+state_id = na.get_state_id(state)
+q_dict = {"locationid":state_id, "datasetid":dset,"enddate":"2001-01-01"}
 station_list = na.get_stations(q_dict)
-station_id = ""
+station_id = None
 for elem in station_list["results"]:
-	if "minneapolis st" in elem["name"].lower():
+	#print(elem)
+	if keyword in elem["name"].lower():
 		station_id = elem["id"]
+		print(elem)
 
-dat = na.get_gsom_data(station_id,1981,2010)
-prcp_sum = 0
-temp_sum = 0
+if station_id == None:
+	print("Station not found")
+	raise Exception("Station not found")
+
+dat = na.get_dset_data(station_id,1981,2010,dset)
+prcp_list = []
+temp_list = []
+# The sum method has limits are results will be wrong if months are skipped
+# Averaging by months or by using the yearly data eliminates these problems
 for elem in dat:
 	if(elem["datatype"] == "PRCP"):
-		prcp_sum += elem["value"]
+		#print(elem)
+		prcp_list.append(elem["value"])
 	elif(elem["datatype"] == "TAVG"):
-		temp_sum += elem["value"]
-avg_prcp = prcp_sum / 30
-avg_temp = temp_sum / (12 * 30)
+		temp_list.append(elem["value"])
+avg_prcp = statistics.mean(prcp_list)
+avg_temp = statistics.mean(temp_list)
 print(f"Avg precip:{avg_prcp}, avg temp: {avg_temp}")
--- noaa_api.py
+++ noaa_api.py
@@ -26,11 +26,11 @@ def get_stations(query_dict):
 	res = requests.get(endpoint + ext, params=query_dict, headers=h)
 	return json.loads(res.content)
 
-def get_gsom_data_backend(station_id, start_date, end_date):
+def get_gsom_data_backend(station_id, start_date, end_date,dataset):
 	ext = "/data"
 	query_dict = {
 		"stationid" : station_id,
-		"datasetid": "GSOM",
+		"datasetid": dataset,
 		"startdate": start_date,
 		"enddate": end_date,
 		"limit": "1000",
@@ -46,14 +46,14 @@ def get_gsom_data_backend(station_id, start_date, end_date):
 		print(res.content)
 	return obj["results"]
 
-def get_gsom_data(station_id, start_year, end_year):
+def get_dset_data(station_id, start_year, end_year,dataset):
 	data_list = []
 	k = 1
 	tmp_start = start_year
 	tmp_end = start_year + k
 	while(tmp_end < end_year + k):
 		print(f"s {tmp_start} e {tmp_end}")
-		out = get_gsom_data_backend(station_id, f"{tmp_start}-01-01",f"{tmp_end}-12-31")
+		out = get_gsom_data_backend(station_id, f"{tmp_start}-01-01",f"{tmp_end}-12-31",dataset)
 		data_list += out
 		tmp_start += k + 1
 		tmp_end += k + 1
