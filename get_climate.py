9759fd6118357b25e337a6dbbe8d7ee78244992f
Logan H <logern5@gmail.com>
2021-03-06 10:35:46 -0600

    Updating API

--- get_climate.py
+++ get_climate.py
@@ -1,1 +1,12 @@
-import noaa_api
+import noaa_api as na
+
+
+state_id = na.get_state_id("Minnesota")
+q_dict = {"locationid":state_id, "datasetid":"GSOM","enddate":"2001-01-01"}
+station_list = na.get_stations(q_dict)
+station_id = ""
+for elem in station_list["results"]:
+	if "minneapolis st" in elem["name"].lower():
+		station_id = elem["id"]
+
+dat = na.get_gsom_data(station_id,1971,2020)
--- noaa_api.py
+++ noaa_api.py
@@ -1,13 +1,71 @@
 import requests
 import json
+import datetime
+import statistics
 
 endpoint = "https://www.ncdc.noaa.gov/cdo-web/api/v2/"
+h = {'token' : 'vFUYfwwlbfAuZDwrXnOSyNvMVpuEtOzR'}
 
-def get_state(state):
+def get_state_id(state):
 	ext = "/locations"
+	query_dict = {
+		"locationcategoryid" : "ST",
+		"limit" : "52"
+	}
+	res = requests.get(endpoint + ext, params=query_dict, headers=h)
+	obj = json.loads(res.content)
+	id = "0"
+	for item in obj["results"]:
+		if item["name"] == state:
+			id = item["id"]
+	return id
 
-def get_station(query_dict):
+def get_stations(query_dict):
 	ext = "/stations"
-	query_dict["limit"] = 1000
-	res = requests.get(endpoint + ext, params=query_dict)
-	return json.reads(res.content)
+	query_dict["limit"] = "1000"
+	res = requests.get(endpoint + ext, params=query_dict, headers=h)
+	return json.loads(res.content)
+
+def get_gsom_data_backend(station_id, start_date, end_date):
+	ext = "/data"
+	query_dict = {
+		"stationid" : station_id,
+		"datasetid": "GSOM",
+		"startdate": start_date,
+		"enddate": end_date,
+		"limit": "1000",
+		"units": "metric"
+		#"datatypeid":"&".join(datatypes)
+	}
+	# We cannot escape the ampersand for the data types
+	#query_str = "&".join(f'{k}={v}' for (k,v) in query_dict.items())
+	#print(query_str)
+	res = requests.get(endpoint + ext,params=query_dict, headers=h)
+	#print("Up here")
+	try:
+		obj = json.loads(res.content)
+		meta = obj["metadata"]["resultset"]
+		new_offset = meta["offset"] + meta["count"]
+		limit = meta["limit"]
+	except:
+		#print(e)
+		print(res.content)
+	#print("Down here")
+	return obj["results"]
+
+def get_gsom_data(station_id, start_year, end_year):
+	#print("&".join(datatypes))
+	data_list = []
+	#tmp_start, tmp_end = 0
+	k = 1
+	tmp_start = start_year
+	tmp_end = start_year + k
+	while(tmp_end < end_year + k):
+		print(f"s {tmp_start} e {tmp_end}")
+		out = get_gsom_data_backend(station_id, f"{tmp_start}-01-01",f"{tmp_end}-12-31")
+		data_list += out
+		tmp_start += k + 1
+		tmp_end += k + 1
+	#out = get_gsom_data_backend
+	return data_list
+
