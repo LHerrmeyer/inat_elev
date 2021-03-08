47d4afbda1447b8bf3fd4fb317f21faf3615a8a6
Logan H <logern5@gmail.com>
2021-03-08 10:12:13 -0600

    Use climate normal data

--- get_climate.py
+++ get_climate.py
@@ -3,33 +3,31 @@ import statistics
 
 state = "Minnesota"
 keyword = "minneapolis st"
-dset = "GSOY"
+dset = "NORMAL_MLY"
 
 state_id = na.get_state_id(state)
-q_dict = {"locationid":state_id, "datasetid":dset,"enddate":"2001-01-01"}
+q_dict = {"locationid":state_id, "datasetid":dset}
 station_list = na.get_stations(q_dict)
 station_id = None
 for elem in station_list["results"]:
-	#print(elem)
 	if keyword in elem["name"].lower():
 		station_id = elem["id"]
-		print(elem)
 
 if station_id == None:
 	print("Station not found")
 	raise Exception("Station not found")
 
-dat = na.get_dset_data(station_id,1981,2010,dset)
+dat = na.get_dset_data_backend(station_id,"2010-01-01","2010-12-31",dset)
 prcp_list = []
 temp_list = []
 # The sum method has limits are results will be wrong if months are skipped
 # Averaging by months or by using the yearly data eliminates these problems
 for elem in dat:
-	if(elem["datatype"] == "PRCP"):
-		#print(elem)
+	if(elem["datatype"] == "MLY-PRCP-NORMAL"):
+		print(elem)
 		prcp_list.append(elem["value"])
-	elif(elem["datatype"] == "TAVG"):
+	elif(elem["datatype"] == "MLY-TAVG-NORMAL"):
 		temp_list.append(elem["value"])
-avg_prcp = statistics.mean(prcp_list)
+avg_prcp = statistics.mean(prcp_list) * 12
 avg_temp = statistics.mean(temp_list)
 print(f"Avg precip:{avg_prcp}, avg temp: {avg_temp}")
--- noaa_api.py
+++ noaa_api.py
@@ -26,7 +26,7 @@ def get_stations(query_dict):
 	res = requests.get(endpoint + ext, params=query_dict, headers=h)
 	return json.loads(res.content)
 
-def get_gsom_data_backend(station_id, start_date, end_date,dataset):
+def get_dset_data_backend(station_id, start_date, end_date,dataset):
 	ext = "/data"
 	query_dict = {
 		"stationid" : station_id,
@@ -37,6 +37,7 @@ def get_gsom_data_backend(station_id, start_date, end_date,dataset):
 		"units": "metric"
 	}
 	res = requests.get(endpoint + ext,params=query_dict, headers=h)
+	#print(f"We are here:{res.content}")
 	try:
 		obj = json.loads(res.content)
 		meta = obj["metadata"]["resultset"]
@@ -53,7 +54,7 @@ def get_dset_data(station_id, start_year, end_year,dataset):
 	tmp_end = start_year + k
 	while(tmp_end < end_year + k):
 		print(f"s {tmp_start} e {tmp_end}")
-		out = get_gsom_data_backend(station_id, f"{tmp_start}-01-01",f"{tmp_end}-12-31",dataset)
+		out = get_dset_data_backend(station_id, f"{tmp_start}-01-01",f"{tmp_end}-12-31",dataset)
 		data_list += out
 		tmp_start += k + 1
 		tmp_end += k + 1
